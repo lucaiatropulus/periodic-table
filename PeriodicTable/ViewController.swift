1df6a9919412f783bed059459d6580fd36576d03
Iatropulus Luca <iatropulus.luca@gmail.com>
2020-08-03 00:48:25 +0300

    Initial Commit

---
+++ PeriodicTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PeriodicTable
+//
+//  Created by Luca Nicolae Iatropulus on 03/08/2020.
+//  Copyright © 2020 Luca Nicolae Iatropulus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
